12295d32614ba68302b90c5e9680a81c9b0d21c1
Mehboob Ahmad <Mosesmehboob@gmail.com>
2020-06-09 15:44:40 +0500

    metadata changes

--- Repository/Models/Leave_Metadata.cs
+++ Repository/Models/Leave_Metadata.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -12,12 +13,25 @@ namespace Repository.Models
         //[DisplayFormat(DataFormatString = "{0: MM/dd/yyyy h:mm:ss tt}")]
         //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0: MM/dd/yyyy h:mm:ss tt}")]
-
+        
+        
+        [DisplayName("Start Date")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         System.DateTime StartDate { get; set; }
-
+        [DisplayName("End Date")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         System.DateTime EndDate { get; set; }
+        [DisplayName("Total Days")]
+        Nullable<int> TotalDays { get; set; }
+
+        [DisplayName("Line Manager 1")]
+        string LineManager1Id { get; set; }
+        [DisplayName("Line Manager 2")]
+        string LineManager2Id { get; set; }
+
+        [DisplayName("Leave Type")]
+        LeaveType LeaveType { get; set; }
+
     }
 
         [MetadataType(typeof(ILeave_MetadataType))]
